5a757f372d358df17801ab0587b1e71c3de6ed81
tokku5552 <physics.0121@gmail.com>
2021-08-01 17:58:09 +0900

    firestoreへの書き込みするところまで完了

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,26 +5,67 @@ import Layout from "../components/Layout";
 import TodoList from "../components/TodoList";
 import { Todo } from "../interfaces";
 import { sampleTodoData } from "../utils/sample-data";
+import { useState, useEffect } from "react";
+import firebase from "../firebase";
 
 type Props = {
   items: Todo[];
 };
 
-const IndexPage = ({ items }: Props) => (
-  <Layout title="Home">
-    <h1>TodoAppSample Next.js</h1>
-    <p>
-      <label>
-        Name:
-        <input type="text" name="name" />
-      </label>{" "}
-      <input type="submit" value="Submit" />
-    </p>
-    <p>
-      <TodoList items={items} />
-    </p>
-  </Layout>
-);
+const IndexPage = ({ items }: Props) => {
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+
+  const data = { name, message };
+
+  useEffect(() => {
+    // firebase.auth().onAuthStateChanged(async (user) => {
+    //   // 匿名ユーザーを作成する
+    //   if (!user) {
+    //     firebase.auth().signInAnonymously();
+    //   }
+    // });
+  });
+
+  // Firestore にデータを登録する関数
+  const createData = async () => {
+    if (!name || !message) {
+      alert("名前とメッセージを入力してください");
+      return;
+    }
+    const db = firebase.firestore();
+    await db.collection("profile").doc(name).set(data);
+    alert("Firestoreにデータを作成できました！");
+  };
+
+  return (
+    <Layout title="Home">
+      <h1>TodoAppSample Next.js</h1>
+      <p>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>{" "}
+        <input type="submit" value="Submit" />
+      </p>
+      <p>
+        <TodoList items={items} />
+      </p>
+      <p>
+        <label>
+          名前：
+          <input value={name} onChange={(e) => setName(e.target.value)} />
+        </label>
+        <label>
+          メッセージ：
+          <input value={message} onChange={(e) => setMessage(e.target.value)} />
+        </label>
+
+        <button onClick={createData}>Firestoreにデータを作成</button>
+      </p>
+    </Layout>
+  );
+};
 
 export const getStaticProps: GetStaticProps = async () => {
   const items: Todo[] = sampleTodoData;
