7f743395ff8a00f561e009c7cc3a266ec45178bb
tokku5552 <physics.0121@gmail.com>
2021-08-11 00:53:02 +0900

    コメント削除

--- src/pages/todos/[id].tsx
+++ src/pages/todos/[id].tsx
@@ -1,5 +1,4 @@
 import { GetStaticProps, GetStaticPaths } from "next";
-
 import { Todo } from "../../interfaces";
 import { sampleTodoData } from "../../utils/sample-data";
 import Layout from "../../components/Layout";
@@ -31,25 +30,18 @@ const StaticPropsDetail = ({ item, errors }: Props) => {
 export default StaticPropsDetail;
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  // Get the paths we want to pre-render based on users
   const paths = sampleTodoData.map((user) => ({
     params: { id: user.id.toString() },
   }));
 
-  // We'll pre-render only these paths at build time.
-  // { fallback: false } means other routes should 404.
   return { paths, fallback: false };
 };
 
-// This function gets called at build time on server-side.
-// It won't be called on client-side, so you can even do
-// direct database queries.
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   try {
     const id = params?.id;
     const item = sampleTodoData.find((data) => data.id === id);
-    // By returning { props: item }, the StaticPropsDetail component
-    // will receive `item` as a prop at build time
+
     return { props: { item } };
   } catch (err) {
     return { props: { errors: err.message } };
